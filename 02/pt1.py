32ef50a1976450ba5831ee1aef1b117ddf4ba9cc
Paul Harman <paul.harman@sainsburys.co.uk>
2021-12-03 00:16:47 +0000

    Second

---
+++ 02/pt1.py
@@ -0,0 +1,27 @@
+import csv
+
+def main():
+    cnt = 0
+
+    try:
+        with open("./input") as f:
+            reader = csv.reader(f)
+            horz = 0
+            depth = 0
+            for line in reader:
+                command = line[0].split(" ")
+                num = int(command[1])
+                if command[0] == "forward":
+                    horz += num
+                if command[0] == "down":
+                    depth += num
+                if command[0] == "up":
+                    depth -= num
+            print(horz * depth)
+
+    finally:
+        f.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 02/pt2.py
@@ -0,0 +1,27 @@
+import csv
+
+def main():
+    cnt = 0
+
+    try:
+        with open("./input") as f:
+            reader = csv.reader(f)
+            horz = depth = aim = 0
+            for line in reader:
+                command = line[0].split(" ")
+                num = int(command[1])
+                if command[0] == "forward":
+                    horz += num
+                    depth += num * aim
+                if command[0] == "down":
+                    aim += num
+                if command[0] == "up":
+                    aim -= num
+            print(horz * depth)
+
+    finally:
+        f.close()
+
+
+if __name__ == "__main__":
+    main()
